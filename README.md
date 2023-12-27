b170e38a79a734cb37e50f69f1203fc7b38b0bd2
BirmaNator <birmandani@gmail.com>
2023-12-27 17:55:00 +0200

    added punchline flask app

---
+++ README.md
@@ -0,0 +1,16 @@
+# Punchline WebApp
+
+This is a simple flask app that returns a punchline an id
+
+
+## How To run
+1) Install requirements
+   1) ``` 
+      pip install -r requirements.txt
+      ```
+2) Run application
+   1) ```
+      python <PATH_TO_SCRIPT>
+      ```
+   2) Consume your punchline
+      In your browser go to http://127.0.0.1
---
+++ app/main.py
@@ -0,0 +1,38 @@
+import requests
+
+from flask import Flask, jsonify
+
+app = Flask(__name__)
+
+
+REQUEST_TIMEOUT = 10
+OFFICIAL_JOKE_API_URL = 'https://official-joke-api.appspot.com/random_joke'
+
+PUNCHLINE_KEY_NAME = 'punchline'
+ID_KEY_NAME = 'id'
+
+
+@app.get("/")
+def root():
+    response = requests.get(OFFICIAL_JOKE_API_URL, verify=False, timeout=REQUEST_TIMEOUT)
+    print(response)
+    try:
+        response.raise_for_status()
+    except requests.exceptions.RequestException as exc:
+        return 'joke url is not responding', 400
+    
+    data = response.json()
+
+    if PUNCHLINE_KEY_NAME not in data or ID_KEY_NAME not in data:
+        return f'{ID_KEY_NAME} / {PUNCHLINE_KEY_NAME} is missing in api response', 400
+    
+    result = {
+        PUNCHLINE_KEY_NAME: data.get(PUNCHLINE_KEY_NAME),
+        ID_KEY_NAME: data.get(ID_KEY_NAME)
+    }
+
+    return jsonify(result)
+
+
+if __name__ == '__main__':
+    app.run()
